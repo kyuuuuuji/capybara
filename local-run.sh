62889ef9d18531ce43b39784f62da545a81e416c
kyuuuuuji <fujikaway0904@hotmail.co.jp>
2016-11-10 16:43:51 +0900

    uid付与忘れ

--- local-run.sh
+++ local-run.sh
@@ -5,7 +5,7 @@ export HUBOT_SLACK_TOKEN=
 export PORT=8080
 
 # redis://<host>:<port>[/<brain_prefix>]
-# export REDIS_URL=redis://localhost:6379/capybara
+export REDIS_URL=redis://localhost:6379/capybara
 
 npm install
 
